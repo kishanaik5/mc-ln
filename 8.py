eb7575bbbbd9d542a75df7fcec8971337364d33b
Kishan K <120300882+kishanaik5@users.noreply.github.com>
2023-08-18 22:46:43 +0530

    8.py

--- 8.py
+++ 8.py
@@ -1,39 +1,31 @@
-# 8  ANN
+# ANN
 import numpy as np
-import tensorflow as tf
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
 
+x = np.array(([2,9],[1,5],[3,6]),dtype= float)
+y = np.array(([92],[86],[89]),dtype= float)
 
-X = np.array(([2, 9],[1, 5],[3, 6]), dtype=float)
-y=np.array(([92],[86],[89]),dtype=float)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+x_train = x/np.amax(x,axis=0)
+y_train = y/100
 
+model = MLPRegressor(hidden_layer_sizes=(32,16),activation='relu',max_iter=100)
 
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+model.fit(x_train,y_train)
 
+y_pred = model.predict(x_test)
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
-    tf.keras.layers.Dense(16, activation='relu'),
-    tf.keras.layers.Dense(1, activation='linear')
-])
+loss = model.loss_
 
+print(f"Loss is {loss}")
+print(f"Actual value {y_test}")
+print(f"predicted value {y_pred}")
 
-model.compile(optimizer='adam', loss='mean_squared_error')
-
-history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
-
-loss = model.evaluate(X_test, y_test)
-print(f"Test loss: {loss}")
-
-plt.plot(history.history['loss'], label='Train')
-plt.plot(history.history['val_loss'], label='Validation')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.title('Learning Curve')
+plt.plot(model.loss_curve_,label = "Train")
+plt.xlabel("epoch")
+plt.ylabel("loss")
 plt.legend()
 plt.show()
